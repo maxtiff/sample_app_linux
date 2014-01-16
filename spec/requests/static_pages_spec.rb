4e62094a6cc54a1d5789b4f317997b7e796f2632
maxtiff <travismay@gmail.com>
2014-01-15 19:22:01 -0600

    Modify titles and spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,9 +11,14 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have the custom title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title(' | Home')
     end
   end
 
@@ -24,9 +29,14 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have the custom title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).not_to have_title(' | Help')
     end
   end
 
@@ -37,9 +47,14 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
 
-    it "should have the title 'About Us'" do
+    it "should have the base title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have the custom title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).not_to have_title(' | About Us')
     end
   end
 
@@ -50,9 +65,14 @@ describe "Static pages" do
       expect(page).to have_content('Contact Us')
     end
 
-    it "should have the title 'Contact Us'" do
+    it "should have the base title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have the custom title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).not_to have_title(' | Contact')
     end
   end
 end
